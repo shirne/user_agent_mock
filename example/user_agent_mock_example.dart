8141dc7bc97589f578b63a7145d6cf4ba04f16f2
shirne <shirne@126.com>
2022-02-25 11:47:17 +0800

    test

--- example/user_agent_mock_example.dart
+++ example/user_agent_mock_example.dart
@@ -3,4 +3,8 @@ import 'package:user_agent_mock/user_agent_mock.dart';
 void main() {
   final userAgent = UserAgent();
   print(userAgent.build());
+  userAgent.system(UASystem.android);
+  print(userAgent.build());
+  userAgent.browser(UABrowser.safari);
+  print(userAgent.build());
 }
--- test/user_agent_mock_test.dart
+++ test/user_agent_mock_test.dart
@@ -5,6 +5,12 @@ void main() {
   group('user agent tests', () {
     final userAgent = UserAgent();
 
-    test('test builder', () {});
+    test('test builder', () {
+      print(userAgent.build());
+      userAgent.system(UASystem.android);
+      print(userAgent.build());
+      userAgent.browser(UABrowser.safari);
+      print(userAgent.build());
+    });
   });
 }
